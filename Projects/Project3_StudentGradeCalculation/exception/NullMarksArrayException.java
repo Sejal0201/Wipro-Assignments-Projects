fa3002738f9014efdd65b86860bb655a63cf596c
SEJAL JAIN <132078607+Sejal0201@users.noreply.github.com>
2025-10-16 22:05:32 +0530

    Mini Projects

---
+++ Projects/Project3_StudentGradeCalculation/exception/NullMarksArrayException.java
@@ -0,0 +1,7 @@
+package Projects.Project3_StudentGradeCalculation.exception;
+
+public class NullMarksArrayException extends Exception {
+    public String toString() {
+        return "marks array is null";
+    }
+}
---
+++ Projects/Project3_StudentGradeCalculation/exception/NullStudentObjectException.java
@@ -0,0 +1,7 @@
+package Projects.Project3_StudentGradeCalculation.exception;
+
+public class NullStudentObjectException extends Exception {
+    public String toString() {
+        return "object is null";
+    }
+}
